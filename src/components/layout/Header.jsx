f06b21d22a335217637be27658f4cac63bad7b43
Sebaur <sebastianurzuar@gmail.com>
2023-10-15 22:42:10 -0300

    header y logos/iconos

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import LogoImagenYoga from '../../../public/images/Logo-ImagenYoga.png'
+import logoyogaramdas from '../../../public/images/logoyogaramdas.png'
 
 export const Header = () => {
     return (
         <header className="header">
-            <div className="logo">
+            {/* <div className="logo">
                 <div className="play"></div>
+            </div> */}
+
+            <div id="logoyoga" className="logoimagenyoga">
+                <img src={LogoImagenYoga} alt="Logo-chacras" />
             </div>
 
-            <h1>MiBlog</h1>
+            <div id="logoyoga" className="logoyogaramdas">
+                <img src={logoyogaramdas} alt="letras-Centro-de-yoga-ramdas" />
+            </div>
+
         </header>
     )
 }
--- src/components/pages/Inicios.jsx
+++ src/components/pages/Inicios.jsx
@@ -3,10 +3,22 @@ import { Link } from 'react-router-dom'
 
 export const Inicios = () => {
     return (
-        <div className="jumbo">
-            <h1>Bienvenido al blog de Sebaur con React!</h1>
-            <p>Blog desarrolado con el MERN Stack (Mongo, Express, React y Nodejs)</p>
-            <Link to='/articulos' className='button'>Ver los artículos</Link>
-        </div>
+        <section className="section" id="section">
+            <div className="jumbo">
+                <article className="content">
+                    <h1 className="title">¿Quiénes Somos?</h1>
+                    <p>Centro de Yoga RamDas especializado en Kundalini Yoga, Profesora Guru Kirn. </p>
+                    <p>Nuestro centro tiene N años de apertura ofreciendo clases tanto a Adultos Mayores, Jovenes y Niños.</p>
+                </article>
+
+                <article className="articulo2">
+                    <h1 className="title">Kundalini Yoga</h1>
+                    <p>Nuestra disciplina de Yoga meditativo que incluye posturas, mantras, y</p>
+                </article>
+
+                {/* ~ Boton para crear articulos ~ */}
+                {/* <Link to='/articulos' classNameName='button'>Ver los artículos</Link> */}
+            </div>
+        </section>
     )
 }
